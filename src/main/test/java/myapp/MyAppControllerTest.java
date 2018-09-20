93de7ae7ba11baeed9ae7902d997d9332f912fbb
Colin Beales <colinbeales@hotmail.com>
2018-09-20 08:13:45 +0100

    Trying to add some tests

---
+++ src/main/test/java/myapp/MyAppControllerTest.java
@@ -0,0 +1,26 @@
+package myapp;
+
+import myapp.ImageService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MyAppControllerTest {
+
+    private MyAppController underTest;
+
+    @Before
+    public void setUp() {
+        this.underTest = new MyAppController();
+    }
+
+    @Test
+    public void testIndex() {
+        String index = this.underTest.index();
+        assertEquals("index.html", index);
+    }
+}
